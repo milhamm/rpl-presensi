7749b6d702c2da955f0dd8a4abb6833e62a99069
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2021-07-18 13:19:18 +0700

    initial antd configuration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+  // optional
+  // modifyVars: {
+  //   // '@primary-color': '#F37335',
+  //   // '@font-family': "'Poppins', sans-serif",
+  // },
+  // optional
+  lessVarsFilePath: './styles/variables.less',
+  // optional
+  // lessVarsFilePathAppendToEndOfContent: false,
+  // optional https://github.com/webpack-contrib/css-loader#object
+  // cssLoaderOptions: {},
+
+  // Other Config Here...
+
+  webpack(config) {
+    return config;
+  },
+});
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+require('antd/dist/antd.less');
+require('../styles/globals.less');
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
